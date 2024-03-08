55b41cfa8bfcc56765b3c87bc6225e60e8c301aa
Alexei <avenge.now.r@mail.ru>
2024-03-08 20:07:04 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((_, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    next();
-  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .addTag('AlexeiIsProgrammer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
